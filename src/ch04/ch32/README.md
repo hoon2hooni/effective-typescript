2787e28ba1ffec802cadb319dbdcddccdfb28ed3
hoon2hooni <sanghoon.engineer@gmail.com>
2023-03-12 14:57:49 +0900

    feat: ch04-item-32

---
+++ src/ch04/ch32/README.md
@@ -0,0 +1,5 @@
+## Item 32: Prefer Unions of Interfaces to Interfaces of Unions
+
+## 아이템 32 유니온의 인터페이스보다는 인터페이스의 유니온을사용하기
+
+유니온의 인터페이스보다 인터페이스의 유니온이 더 정확하고 타입스크립트가 이해하기도 좋습니다.
---
+++ src/ch04/ch32/index.ts
@@ -0,0 +1,96 @@
+{
+  {
+    interface Layer {
+      layout: FillLayout | LineLayout | PointLayout;
+      paint: FillPaint | LinePaint | PointPaint;
+    }
+  }
+  {
+    interface FillLayer {
+      layout: FillLayout;
+      paint: FillPaint;
+    }
+    interface LineLayer {
+      layout: LineLayout;
+      paint: LinePaint;
+    }
+    interface PointLayer {
+      layout: PointLayout;
+      paint: PointPaint;
+    }
+    type Layer = FillLayer | LineLayer | PointLayer;
+  }
+  {
+    interface FillLayer {
+      type: "fill";
+      layout: FillLayout;
+      paint: FillPaint;
+    }
+    interface LineLayer {
+      type: "line";
+      layout: LineLayout;
+      paint: LinePaint;
+    }
+    interface PointLayer {
+      type: "paint";
+      layout: PointLayout;
+      paint: PointPaint;
+    }
+    type Layer = FillLayer | LineLayer | PointLayer;
+    function drawLayer(layer: Layer) {
+      if (layer.type === "fill") {
+        const { paint } = layer; // Type is FillPaint
+        const { layout } = layer; // Type is FillLayout
+      } else if (layer.type === "line") {
+        const { paint } = layer; // Type is LinePaint
+        const { layout } = layer; // Type is LineLayout
+      } else {
+        const { paint } = layer; // Type is PointPaint
+        const { layout } = layer; // Type is PointLayout
+      }
+    }
+    interface Person {
+      name: string;
+      birth?: {
+        place: string;
+        date: Date;
+      };
+    }
+
+    const alanT: Person = {
+      name: "Alan Turing",
+      birth: {
+        place: "London",
+      },
+      // ~~~~ Property 'date' is missing in type
+      // //
+    };
+
+    function eulogize(p: Person) {
+      console.log(p.name);
+      const { birth } = p;
+      if (birth) {
+        console.log(`was born on ${birth.date} in ${birth.place}.`);
+      }
+    }
+
+    {
+      interface Name {
+        name: string;
+      }
+      interface PersonWithBirth extends Name {
+        placeOfBirth: string;
+        dateOfBirth: Date;
+      }
+
+      type Person = Name | PersonWithBirth;
+
+      function eulogize(p: Person) {
+        if ("placeOfBirth" in p) {
+          p; // Type is PersonWithBirth
+          const { dateOfBirth } = p; // OK, type is Date
+        }
+      }
+    }
+  }
+}
